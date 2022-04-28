72c1c747a289d465af495fb1e11b65b474d6c7eb
Wei Wang <wwtyler@gmail.com>
2022-04-28 19:03:19 +0800

    角色初始状态修改1.1
    #1 角色出生自带盒子
    #2 角色出生初始耐力x3
    #3 角色出生自带乔丹之石
    #4 角色升级每级的技能点数

--- CharStats/mod.js
+++ CharStats/mod.js
@@ -5,9 +5,19 @@ const charstats = D2RMM.readTsv(charstatsFilename);
 charstats.rows.forEach((row) => {
 
   if (row.class !== 'Expansion') {
+    //角色出生自带盒子
     row.item6 = 'box';
-    row.stamina = Math.floor(row.stamina * 3);
     row.item6count = 1;
+
+    //角色出生初始耐力x3
+    row.stamina = Math.floor(row.stamina * 3);
+
+    //角色出生自带乔丹之石
+    row.item7 = 'The Stone of Jordan';
+    row.item7count = 1;
+
+    //角色升级每级的技能点数
+    row.SkillsPerLevel = 2;
   }
 
 });
