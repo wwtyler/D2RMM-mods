a45536aa3a236b4a199927c4e95521986e93e604
维 王 <wwtyler@gmail.com>
2022-05-22 16:37:13 +0800

    对菜单公式代码进行了重构。

--- CubemainRecipes/mod.js
+++ CubemainRecipes/mod.js
@@ -1,12 +1,44 @@
 ///////////////////////////////////////////////////////////////
-
 const cubemainFilename = 'global\\excel\\cubemain.txt';
 const cubemain = D2RMM.readTsv(cubemainFilename);
-const EQUIP_TYPES = ['weap', 'armo'];
-const MAGIC_TYPES = ['nor', 'hiq', 'mag', 'rar', 'set', 'uni']
+
+const ETH_TYPES = ['weap', 'armo'];
+const ALL_RARITY = ['low', 'nor', 'hiq', 'mag', 'rar', 'set', 'uni', 'crf', 'tmp']
 const ALL_UNIQUE_TYPES = ['weap', 'armo', 'amu', 'rin', 'jew', 'char'];
 const ALL_PG_CODES = ['gpv', 'gpy', 'gpb', 'gpg', 'gpr', 'gpw', 'skz'];
 const ALL_PG_S_CODES = ['zpv', 'zpy', 'zpb', 'zpg', 'zpr', 'zpw', 'zkz'];
+const TRI_FORGE_RARITY = {
+  mag: '魔法',
+  rar: '稀有',
+  uni: '暗金',
+  set: '套装'
+};
+const JEW_FORGE_TYPES = [
+  ['rin', '戒指'],
+  ['amu', '项链']
+];
+const REROLL_RARITY = [
+  ['mag', '魔法'],
+  ['rar', '稀有'],
+  ['uni', '暗金'],
+  ['set', '套装']
+];
+const REROLL_TYPES = [
+  ['rin', '戒指'],
+  ['amu', '项链'],
+  ['char', '护符'],
+  ['jew', '珠宝'],
+  ['weap', '武器'],
+  ['armo', '防具']
+];
+const EXTRACT_TYPES = [
+  ['rin', '戒指'],
+  ['amu', '项链'],
+  ['char', '护符'],
+  ['jew', '珠宝'],
+  ['weap', '武器'],
+  ['armo', '防具']
+];
 
 const scRecipe = {
   description: `enhance sc `,
@@ -19,7 +51,7 @@ const scRecipe = {
   'mod 1': 'addxp',
   'mod 1 min': 40,
   'mod 1 max': 50,
-  '*eol': 0,
+  '*eol\r': 0,
 };
 
 // cubemain.rows.push({
@@ -35,11 +67,11 @@ cubemain.rows.push({
   version: 100,
   numinputs: 2,
   'input 1': '"char"',
-  'input 2': `r01`,
+  'input 2': `run2`,
   plvl: 99,
   ilvl: 99,
-  output: 'The Stone of Jordan',
-  '*eol': 0,
+  output: "Andariel's Visage",
+  '*eol\r': 0,
 });
 
 cubemain.rows.push({
@@ -58,7 +90,7 @@ cubemain.rows.push({
   'mod 1': 'allskills',
   'mod 1 min': 1,
   'mod 1 max': 1,
-  '*eol': 0,
+  '*eol\r': 0,
 });
 
 cubemain.rows.push({
@@ -80,7 +112,7 @@ cubemain.rows.push({
   'mod 2': 'innernum',
   'mod 2 min': 1,
   'mod 2 max': 1,
-  '*eol': 0,
+  '*eol\r': 0,
 });
 
 cubemain.rows.push({
@@ -93,7 +125,7 @@ cubemain.rows.push({
   plvl: 99,
   ilvl: 99,
   output: 'useitem,suf=754',
-  '*eol': 0,
+  '*eol\r': 0,
 });
 
 cubemain.rows.push({
@@ -106,7 +138,7 @@ cubemain.rows.push({
   plvl: 99,
   ilvl: 99,
   output: 'useitem,suf=756',
-  '*eol': 0,
+  '*eol\r': 0,
 });
 cubemain.rows.push({
   description: `test FFF`,
@@ -118,20 +150,18 @@ cubemain.rows.push({
   plvl: 99,
   ilvl: 99,
   output: 'usetype,mag,suf=748',
-  '*eol': 0,
+  '*eol\r': 0,
 });
 
 cubemain.rows.push({
   description: `test GGG`,
   enabled: 1,
   version: 100,
-  numinputs: 2,
-  'input 1': '"weap"',
-  'input 2': `r07`,
-  plvl: 99,
-  ilvl: 99,
-  output: 'usetype,mag,suf=750',
-  '*eol': 0,
+  numinputs: 3,
+  'input 1': '"gem3,qty=3"',
+  lvl: 99,
+  output: 'gem4,qty=1',
+  '*eol\r': 0,
 });
 // 三蓝色戒指合成蓝色项链	1			100					3	"rin,mag,qty=3"							"amu,mag"	99	0
 // 三蓝色项链合成蓝色戒指	1			100					3	"amu,mag,qty=3"							"rin,mag"	99	0
@@ -153,57 +183,34 @@ cubemain.rows.forEach((row) => {
   }
 });
 
-cubemain.rows.push({
-  description: `三mag戒指合成亮金戒指`,
-  enabled: 1,
-  version: 100,
-  numinputs: 3,
-  'input 1': 'rin,mag,qty=3',
-  plvl: 99,
-  output: 'rin,rar',
-  '*eol': '0',
+
+//三个项链、戒指REROLL。
+[
+  ['mag', 'rar'],
+  ['rar', 'rar'],
+  ['uni', 'uni']
+].forEach(([fromRarity, toRarity]) => {
+  const fromRarityLabel = TRI_FORGE_RARITY[fromRarity];
+  const toRarityLabel = TRI_FORGE_RARITY[toRarity];
+
+  JEW_FORGE_TYPES.forEach(([type, typeLabel]) => {
+    //'reg' = If the function has “usetype” and if the item is a Unique, then regenerate/reroll the Unique
+    const outputString = fromRarity === 'uni' ? 'useitem,reg' : `${type},${toRarity}`;
+    const recipe = {
+      description: `${fromRarityLabel}${typeLabel} 合成(FORGE) ${toRarityLabel}${typeLabel}`,
+      enabled: 1,
+      version: 100,
+      numinputs: 3,
+      'input 1': `${type},${fromRarity},qty=3`,
+      output: `${outputString}`,
+      ilvl: 100, // preserve item level
+      '*eol\r': 0,
+    };
+    cubemain.rows.push(recipe);
+  });
 });
 
-cubemain.rows.push({
-  description: `三mag项链合成亮金项链`,
-  enabled: 1,
-  version: 100,
-  numinputs: 3,
-  'input 1': 'amu,mag,qty=3',
-  plvl: 99,
-  output: 'amu,rar',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `三亮金项链合成亮金项链`,
-  enabled: 1,
-  version: 100,
-  numinputs: 3,
-  'input 1': 'amu,rar,qty=3',
-  plvl: 99,
-  output: 'amu,rar',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `三暗金戒指合成暗金戒指`,
-  enabled: 1,
-  version: 100,
-  numinputs: 3,
-  'input 1': 'rin,uni,qty=3',
-  plvl: 99,
-  output: 'rin,uni',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `三暗金项链合成暗金项链`,
-  enabled: 1,
-  version: 100,
-  numinputs: 3,
-  'input 1': 'amu,uni,qty=3',
-  plvl: 99,
-  output: 'amu,uni',
-  '*eol': '0',
-});
+//项链戒指merge公式。
 cubemain.rows.push({
   description: `亮金项链戒指合成亮金珠宝`,
   enabled: 1,
@@ -212,7 +219,7 @@ cubemain.rows.push({
   'input 1': 'amu,rar',
   'input 2': 'rin,rar',
   output: 'jew,rar',
-  '*eol': '0',
+  '*eol\r': '0',
 });
 cubemain.rows.push({
   description: `魔法项链戒指合成魔法珠宝`,
@@ -222,135 +229,90 @@ cubemain.rows.push({
   'input 1': 'amu,mag',
   'input 2': 'rin,mag',
   output: 'jew,mag',
-  '*eol': '0',
+  '*eol\r': '0',
 });
 
-cubemain.rows.push({
-  description: `洗护符`,
-  enabled: 1,
-  version: 100,
-  numinputs: 4,
-  'input 1': 'char',
-  'input 2': 'gem4,qty=3',
-  plvl: 50,
-  ilvl: 50,
-  output: 'usetype,mag',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `洗珠宝`,
-  enabled: 1,
-  version: 100,
-  numinputs: 4,
-  'input 1': 'jew',
-  'input 2': 'gem4,qty=3',
-  plvl: 50,
-  ilvl: 50,
-  output: 'usetype,rar',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `洗戒指`,
-  enabled: 1,
-  version: 100,
-  numinputs: 4,
-  'input 1': 'rin',
-  'input 2': 'gem4,qty=3',
-  plvl: 50,
-  ilvl: 50,
-  output: 'usetype,rar',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `洗项链`,
-  enabled: 1,
-  version: 100,
-  numinputs: 4,
-  'input 1': 'amu',
-  'input 2': 'gem4,qty=3',
-  plvl: 50,
-  ilvl: 50,
-  output: 'usetype,rar',
-  '*eol': '0',
-});
-
-cubemain.rows.push({
-  description: `洗防具`,
-  enabled: 1,
-  version: 100,
-  numinputs: 4,
-  'input 1': 'armo',
-  'input 2': 'gem4,qty=3',
-  plvl: 50,
-  ilvl: 50,
-  output: 'usetype,rar',
-  '*eol': '0',
-});
-cubemain.rows.push({
-  description: `洗武器`,
-  enabled: 1,
-  version: 100,
-  numinputs: 4,
-  'input 1': 'weap',
-  'input 2': 'gem4,qty=3',
-  plvl: 50,
-  ilvl: 50,
-  output: 'usetype,rar',
-  '*eol': '0',
-});
-
-ALL_UNIQUE_TYPES.forEach((uniqueType) => {
-  const uniqueIndex = ALL_UNIQUE_TYPES.indexOf(uniqueType);
-  // Math.floor(Math.random()*7);
-  const pgSCode = ALL_PG_S_CODES[uniqueIndex];
-  cubemain.rows.push({
-    description: `暗金萃取-${uniqueType}`,
-    enabled: 1,
-    version: 100,
-    numinputs: 2,
-    'input 1': `${uniqueType},uni`,
-    'input 2': 'rvs',
-    lvl: 99,
-    output: `${pgSCode},qty=1`,
-    '*eol': '0',
+//各种装备REROLL
+REROLL_TYPES.forEach(([type, typeLabel]) => {
+  REROLL_RARITY.forEach(([rarity, rarityLabel]) => {
+    //'reg' = If the function has “usetype” and if the item is a Unique, then regenerate/reroll the Unique
+    const outputString = rarity === 'uni' ? 'useitem,reg' : `usetype,${rarity}`;
+    const inputCost = 'gem4,qty=3';
+    const recipe = {
+      description: `洗（REROLL）${rarityLabel}${typeLabel}`,
+      enabled: 1,
+      version: 100,
+      numinputs: 4,
+      'input 1': `${type},${rarity}`,
+      'input 2': `${inputCost}`,
+      output: `${outputString}`,
+      ilvl: 100, // preserve item level
+      '*eol\r': 0,
+    };
+    cubemain.rows.push(recipe);
   });
 });
 
 
-EQUIP_TYPES.forEach((equipType) => {
-  MAGIC_TYPES.forEach((magicType) => {
+[
+  ['uni', '暗金'],
+  ['set', '套装']
+].forEach(([rarity, rarityLabel]) => {
+  EXTRACT_TYPES.forEach(([uniqueType, typeLabel]) => {
+    const uniqueIndex = ALL_UNIQUE_TYPES.indexOf(uniqueType);
+    const pgSCode = ALL_PG_S_CODES[uniqueIndex];
+    cubemain.rows.push({
+      description: `${rarityLabel}${typeLabel}萃取（EXTRACT）`,
+      enabled: 1,
+      version: 100,
+      numinputs: 2,
+      'input 1': `${uniqueType},${rarity}`,
+      'input 2': 'rvs',
+      lvl: 99,
+      ilvl: 99, // preserve item level
+      output: `gem4,qty=1`,
+      'output b': `rvs`,
+      '*eol\r': '0',
+    });
+  });
+});
+
+ETH_TYPES.forEach((equipType) => {
+  ALL_RARITY.forEach((magicType) => {
     const description = "制作无形" + `${equipType}-${magicType}`;
     cubemain.rows.push({
       description: description,
       enabled: 1,
       version: 100,
-      numinputs: 3,
+      numinputs: 2,
       'input 1': `${equipType},${magicType}`,
-      'input 2': "isc",
-      'input 3': "r25",
-      plvl: 80,
+      'input 2': "r25",
+      // 'input 3': "isc",
+      ilvl: 100, // preserve item level
       output: 'useitem',
       'mod 1': 'ethereal',
-      '*eol': '0',
+      'mod 1 min': 1,
+      'mod 1 max': 1,
+      '*eol\r': '0',
     });
   });
 });
 
 //底材萃取
-EQUIP_TYPES.forEach((equipType) => {
-  MAGIC_TYPES.forEach((magicType) => {
+ETH_TYPES.forEach((equipType) => {
+  ALL_RARITY.forEach((magicType) => {
     const description = "底材萃取" + `${equipType}-${magicType}`;
     cubemain.rows.push({
       description: description,
       enabled: 1,
       version: 100,
-      numinputs: 3,
+      numinputs: 2,
       'input 1': `${equipType},${magicType}`,
-      'input 2': "tsc",
-      'input 3': "r18",
-      plvl: 99,
+      'input 2': "r18",
+      // 'input 3': "tsc",
+      ilvl: 100, // preserve item level
       output: 'usetype,bas',
-      '*eol': '0',
+      '*eol\r': '0',
     });
   });
 });
@@ -372,7 +334,7 @@ itemstatcost.rows.forEach((row) => {
     row['descstrneg'] = 'innernumdesc';
     row['descpriority'] = 159;
     row['descfunc'] = 19;
-    row['*eol'] = 0;
+    row['*eol\r'] = 0;
   }
 });
 
@@ -385,7 +347,7 @@ properties.rows.push({
   func1: 1,
   stat1: 'innernum',
   '*Tooltip': '+# item inner number for cubemain use.',
-  '*eol': 0,
+  '*eol\r': 0,
 });
 
 D2RMM.writeTsv(itemstatcostFilename, itemstatcost);
