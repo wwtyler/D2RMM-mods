8d615b2737733fc4617e498375b5c6240e8ebbd4
维 王 <wwtyler@gmail.com>
2022-05-22 16:36:04 +0800

    rvs rvl 商店可售。

--- RunesOnSale/mod.js
+++ RunesOnSale/mod.js
@@ -2,13 +2,25 @@
 const miscFilename = 'global\\excel\\misc.txt';
 const misc = D2RMM.readTsv(miscFilename);
 
-const NPC_NAMES = ['Akara', 'Fara', 'Lysander', 'Ormus', 'Halbu', 'Malah', 'Anya','Jamella'];
+const NPC_NAMES = ['Akara', 'Fara', 'Lysander', 'Ormus', 'Halbu', 'Malah', 'Anya', 'Jamella'];
 
 misc.rows.forEach((row) => {
   //rarity < 10 相当于 r01~r12
-  if (row.type === "rune" && row.rarity < 10) {
+  // zod = 30
+  if (row.type === "rune" && row.rarity < 30) {
     row.PermStoreItem = 1;
-    row.spawnable = 1
+    row.spawnable = 1;
+    NPC_NAMES.forEach((npc) => {
+      row[`${npc}min`] = 2;
+      row[`${npc}max`] = 4;
+      row[`${npc}MagicMin`] = 2;
+      row[`${npc}MagicMax`] = 4;
+    });
+  }
+  if (row.code === "rvs" || row.code === "rvl") {
+    row.PermStoreItem = 1;
+    row.spawnable = 1;
+    row.multibuy =1 ;
     NPC_NAMES.forEach((npc) => {
       row[`${npc}min`] = 2;
       row[`${npc}max`] = 4;
