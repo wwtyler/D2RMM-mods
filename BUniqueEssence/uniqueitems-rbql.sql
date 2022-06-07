19d50dbe131e289373c611d3ad7cad9480eb24e4
维 王 <wwtyler@gmail.com>
2022-06-07 23:18:25 +0800

    20220607

--- BUniqueEssence/uniqueitems-rbql.sql
+++ BUniqueEssence/uniqueitems-rbql.sql
@@ -1,8 +1,7 @@
-SELECT a1,a2,a11,a8,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32,a33,a34,a35,a36,a37,a38,a39,a40,a41  
-where  a8>70  
-order by parseInt(a8) desc 
+SELECT a1,a2,a11,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32,a33,a34,a35,a36,a37,a38,a39,a40,a41  
+where  a8>50  
+order by parseInt(a2) 
 WITH (header)
-
 
 select a2, ARRAY_AGG(a8, v => v.sort().slice(0, 5)) 
 group by a2 
